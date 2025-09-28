3331ef79fc827cd21363a5e5a1d50a0ff0254c20
Shibee <116005554+shibeedev@users.noreply.github.com>
2025-09-28 19:43:19 +0700

    Add common use components
    Analytics and seo boilerplate

---
+++ src/lib/Seo.svelte
@@ -0,0 +1,36 @@
+<script>
+  import { page } from '$app/stores';
+  import { derived } from 'svelte/store';
+
+  // Default values
+  let defaultTitle = "OpenArena - PvP Game for any NFT, unleash unique skills from each NFT trait, and solo others for staked $U2U";
+  let defaultKeyword = "OpenArena, PvP, Gamefi, NFT, U2U Network"
+  let defaultDescription = "OpenArena - PvP Game for any NFT, unleash unique skills from each NFT trait, and solo others for staked $U2U"
+  // Base URL for your site
+  const baseUrl = "https://openarena.cc";
+
+  // Derive the full URL for the current page
+  const url = derived(page, $page => `${baseUrl}${$page.url.pathname}`);
+
+  // Reactive variables for SEO
+  export let title;
+  export let description;
+  export let keywords;
+  // Update values based on the page
+  $: {
+    title = title || defaultTitle;
+    description = description || defaultDescription;
+    keywords  = keywords || defaultKeyword;
+  }
+</script>
+
+<svelte:head>
+  <!-- Canonical Tag -->
+  <link rel="canonical" href={$url} />
+
+  <!-- Primary Meta Tags -->
+  <title>{title}</title>
+  <meta name="title" content={title} />
+  <meta name="description" content={description} />
+</svelte:head>
+
